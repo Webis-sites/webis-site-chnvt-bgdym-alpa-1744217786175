f0e0ba8d02ccff60db66d99fb5a38c727b26e4d8
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-09 19:59:10 +0300

    Update next.config.js with domain res.cloudinary.com

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'via.placeholder.com', 'alpha-clothing.com'],
+    domains: ['images.unsplash.com', 'via.placeholder.com', 'alpha-clothing.com', 'res.cloudinary.com'],
   },
 };
 
