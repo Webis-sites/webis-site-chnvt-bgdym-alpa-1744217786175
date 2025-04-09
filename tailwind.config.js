a84e104c8b3535ebb3e7fcf27d2d753f6e0d3abc
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-09 19:59:11 +0300

    Update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,15 @@
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
       ],
-      theme: {
-        extend: {
-          colors: {
+      'theme': {
+        'extend': {
+          'colors': {
             primary: '#FFEEAD',
             secondary: '#9B786F'
-          },
-          backgroundImage: {
+          ,
+        surface: "#F8F9FA"
+      },
+          'backgroundImage': {
             'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
             'gradient-conic':
               'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
